8ebd0b2f77f70252d1de55b479dbe347c6c326de
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2024-04-25 15:44:24 +0300

    feat: Template sync (#4)
    * feat: Add template sync
    
    * fix: Failing tests
    
    * chore: Sync changes from the template (#3)
    
    Co-authored-by: oprypkhantc <54406427+oprypkhantc@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: package-sync[bot] <165811605+package-sync[bot]@users.noreply.github.com>

--- src/RestrictCommandServiceProvider.php
+++ src/RestrictCommandServiceProvider.php
@@ -11,7 +11,7 @@ class RestrictCommandServiceProvider extends ServiceProvider
 	public function boot(Dispatcher $events): void
 	{
 		$this->publishes([
-			__DIR__ . '/../config/restricted-commands.php' => config_path('restricted-commands.php'),
+			__DIR__ . '/../resources/config/restricted-commands.php' => config_path('restricted-commands.php'),
 		]);
 
 		$events->listen(CommandStarting::class, [StopRestrictedCommandExecution::class, 'handle']);
@@ -22,7 +22,7 @@ class RestrictCommandServiceProvider extends ServiceProvider
 		parent::register();
 
 		$this->mergeConfigFrom(
-			__DIR__ . '/../config/restricted-commands.php',
+			__DIR__ . '/../resources/config/restricted-commands.php',
 			'restricted-commands'
 		);
 	}
